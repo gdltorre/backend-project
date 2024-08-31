6776b5da93320163b273f9462a37659ed5ed5598
gdltorre <97696452+gdltorre@users.noreply.github.com>
2024-08-31 16:54:21 -0700

    Completed

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { getConnection } from 'typeorm';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: ['error', 'warn'],
   });
 
   /*try {
